ca1f3a23c63bf8d35603dcc925860888da7332a5
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2020-12-05 10:54:34 -0500

    Day 1 part 2

--- src/main/java/org/aoc/day1/day1.java
+++ src/main/java/org/aoc/day1/day1.java
@@ -2,18 +2,37 @@ package org.aoc.day1;
 
 import org.aoc.utils.InputReader;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class day1 {
     public static void main(String[] args) {
         ArrayList<Integer> input = InputReader.convertFileContentToInt(InputReader.readInputFile("day1"));
 
+        //part1(input);
+        part2(input);
+    }
+
+    private static void part1(ArrayList<Integer> input) {
         input.forEach(num1 -> {
             input.forEach(num2 -> {
                 if (num1 + num2 == 2020) {
-                    System.out.println("Result : " + num1 * num2);
+                    System.out.println("Part 1 : " + num1 * num2);
                     System.exit(0);
                 }
+            });
+        });
+    }
+
+    private static void part2(ArrayList<Integer> input) {
+        input.forEach(num1 -> {
+            input.forEach(num2 -> {
+                input.forEach(num3 -> {
+                    if (num1 + num2 + num3 == 2020) {
+                        System.out.println("Part 2 : " + num1 * num2 * num3);
+                        System.exit(0);
+                    }
+                });
             });
         });
     }
